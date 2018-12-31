c6b2696cb52ac32cca20bb99ff9a669102774134
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-12-31 18:18:49 +0100

    managed to get some dependency graph working

--- example/depgraph.hs
+++ example/depgraph.hs
@@ -1,25 +1,31 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns        #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
 {-| Compute a dependency graph over a list of python files -}
 import Control.Lens
 import Control.Lens.Plated
 import Control.Monad (when, (<=<))
+import Data.Function (on)
+import Data.Graph (Graph, Vertex, edges, graphFromEdges)
 import qualified Data.List as List
 import Data.Monoid ((<>))
+import Data.Ord (comparing)
 import Data.Text.Lazy (unpack)
 import Language.Python.Internal.Lexer (indentation, logicalLines, nested,
                                        tokenize)
 import Language.Python.Internal.Optics
 import Language.Python.Internal.Parse (module_, runParser)
-import Language.Python.Internal.Render (renderModule, showRenderOutput)
+import Language.Python.Internal.Render (renderModule, showExpr,
+                                        showRenderOutput)
 import Language.Python.Internal.Syntax
 import Language.Python.Internal.Syntax.Module (Module)
 import Language.Python.Internal.Token ()
 import System.Environment (getArgs)
 import System.Exit
+import qualified System.FilePath as File
 import qualified Text.Trifecta as Trifecta
 
 
@@ -64,21 +70,67 @@ main :: IO ()
 main = do
   args <- getArgs
   modules <- traverse readPy args
-  let deps :: [String] = fmap _identValue $ buildDependencyGraph modules
-  mapM_ putStrLn deps
+  let calls = concatMap (uncurry buildDependencyGraph) (zip args modules)
+  putStrLn $ displayGraph $ filter unwantedCalls $ calls
 
-
--- type Node = String
-
--- type Graph = [ Node, [ Node ] ]
-
-buildDependencyGraph :: [ Module '[] Trifecta.Caret ] -> [ Ident '[] Trifecta.Caret ]
-buildDependencyGraph = allDefinitions . allExpressions
-
-allDefinitions :: [ Statement '[] Trifecta.Caret ] -> [ Ident '[] Trifecta.Caret ]
-allDefinitions = fmap _getIdent . concatMap (toListOf _Fundef)
+unwantedCalls :: ( Node, Node) -> Bool
+unwantedCalls (s,t) = s `notElem` unwanted && t `notElem` unwanted
   where
-    _getIdent (_,_,_,_,i,_,_,_,_,_) = i
+    unwanted = [ aCast, aJob_Start]
 
-allExpressions :: [ Module '[] Trifecta.Caret ] -> [ Statement '[] Trifecta.Caret ]
-allExpressions = concatMap (toListOf _Statements)
+data Node = Node { _namespace :: String
+                 , _function  :: String
+                 }
+  deriving (Eq, Ord, Show)
+
+aCast :: Node
+aCast = Node "cvm" "cast"
+
+aJob_Start :: Node
+aJob_Start = Node "cvm" "job_start"
+
+display :: Node -> String
+display (Node ns fn) = ns <> "." <> fn
+
+displayGraph :: [(Node, Node)]
+             -> String
+displayGraph graph =
+  unlines $ hdr <> (fmap mkEdges $ List.nub $ List.sort $ graph) <> ftr
+  where
+    mkEdges (s, t) = "  \"" <> display s <> "\" -> \"" <> display t <> "\";"
+    hdr = [ "digraph G {" ]
+    ftr = [ "}" ]
+
+buildDependencyGraph :: FilePath -> Module '[] Trifecta.Caret -> [ (Node, Node) ]
+buildDependencyGraph fp = allCalls ns . allDefinitions . allExpressions
+  where
+    ns = File.takeBaseName fp
+
+allCalls :: String
+         -> [(Ident '[] Trifecta.Caret,
+               Suite '[] Trifecta.Caret)]
+         -> [(Node, Node)]
+allCalls ns = concatMap (uncurry $ allCallExprs ns)
+
+allCallExprs :: String
+             -> Ident '[] Trifecta.Caret
+             -> Suite '[] Trifecta.Caret
+             -> [(Node, Node)]
+allCallExprs ns (_identValue -> caller) body =
+ let calls = toListOf (_Exprs._Call._2) body
+ in fmap ( \c -> (Node ns caller, extractNode c)) calls
+ where
+   builtins = [ "Identifier", "cast", "PostTxArgs", "ChannelName" ]
+   extractNode (Ident _ (_identValue -> called))
+     | called `notElem` builtins = Node ns called
+     | otherwise = Node "cvm" called
+   extractNode (Deref _ l _ identifier) = Node (unpack $ showExpr l) $ _identValue identifier
+   extractNode e = Node "<?>" (unpack $ showExpr e)
+
+allDefinitions :: [ Statement '[] Trifecta.Caret ] -> [ (Ident '[] Trifecta.Caret, Suite '[] Trifecta.Caret) ]
+allDefinitions = concatMap (toListOf (_Fundef . to _getFun))
+  where
+    _getFun (_,_,_,_,i,_,_,_,_,s) = (i,s)
+
+allExpressions :: Module '[] Trifecta.Caret -> [ Statement '[] Trifecta.Caret ]
+allExpressions = toListOf _Statements
