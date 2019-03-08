ef4dc44ea172f2d579955c5fd8a301e60512a7cb
Arnaud Bailly <arnaud.oqube@gmail.com>
2019-03-08 07:27:19 +0100

    find schemas and contract references

--- example/depgraph.hs
+++ example/depgraph.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns        #-}
@@ -11,11 +12,13 @@ import Control.Monad (when, (<=<))
 import Data.Function (on)
 import Data.Graph (Graph, Vertex, edges, graphFromEdges)
 import qualified Data.List as List
-import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty (NonEmpty, toList)
+import Data.Maybe (catMaybes)
 import Data.Monoid ((<>))
 import Data.Ord (comparing)
 import Data.Text (Text, pack, unpack)
 import Data.Validation (Validation, toEither)
+import Debug.Trace (trace)
 import Language.Python.Internal.Lexer (SrcInfo)
 import Language.Python.Internal.Render (renderModule, showRenderOutput)
 import Language.Python.Internal.Token ()
@@ -129,11 +132,19 @@ allCallExprs ns (_identValue -> caller) body =
  in fmap ( \c -> (Node ns caller, extractNode c)) calls
  where
    builtins = [ "Identifier", "cast", "PostTxArgs", "ChannelName" ]
+
    extractNode (Ident _ (_identValue -> called))
      | called `notElem` builtins = Node ns called
      | otherwise = Node "cvm" called
    extractNode (Deref _ l _ identifier) = Node (unpack $ showExpr l) $ _identValue identifier
    extractNode e = Node "<?>" (unpack $ showExpr e)
+
+allReferences ::  [ Statement '[] SrcInfo ] -> [ Expr '[] SrcInfo ]
+allReferences = filter isDeref .
+                concatMap (universeOn (_Exprs))
+  where
+    isDeref (Deref _ _ _ _) = True
+    isDeref _               = False
 
 allDefinitions :: [ Statement '[] SrcInfo ] -> [ (Ident '[] SrcInfo, Suite '[] SrcInfo) ]
 allDefinitions = concatMap (toListOf (_Fundef . to makeFun))
@@ -142,3 +153,69 @@ allDefinitions = concatMap (toListOf (_Fundef . to makeFun))
 --(Fundef _ decos idnt asyncWs ws1 name ws2 params ws3 mty s)
 allExpressions :: Module '[] SrcInfo -> [ Statement '[] SrcInfo ]
 allExpressions = toListOf _Statements
+
+toplevelAssignments :: [ Statement '[] SrcInfo ] -> [ Statement '[] SrcInfo ]
+toplevelAssignments = filter isAssignment
+  where
+    isAssignment  (SmallStatement _ (MkSmallStatement (Assign _ _ _) _ _ _ _)) = True
+    isAssignment  _ = False
+
+-- | Get contracts' imports
+contractDefinitions :: [ Statement '[] SrcInfo ] -> [ Statement '[] SrcInfo ]
+contractDefinitions = filter contractImports
+  where
+    contractImports (SmallStatement _ (MkSmallStatement asss _ _ _ _)) =
+      case asss ^? _Exprs._Call.callFunction._Ident.identValue of
+        Just "Contract" -> True
+        _               -> False
+    contractImports _ = False
+
+data ContractRef = ContractRef { _name :: Text, _version :: Text }
+  deriving (Eq, Show)
+
+-- | Extract all contracts references from the given list of statements.
+--
+-- Given the following source code,
+-- @
+-- rat = Contract('rational', '1.0.0', direct=True)
+-- investors = Contract('investors', '1.0.0')
+-- @
+--
+-- This function will return
+-- @
+-- [ ("rat", ContractRef {_name = "'rational'", _version = "'1.0.0'"})
+-- , ("investors", ContractRef {_name = "'investors'", _version = "'1.0.0'"})
+-- ]
+-- @
+--
+-- This is needed to chase external calls to other contracts while
+-- chasing references within a contract.
+contractsRefs :: [ Statement '[] SrcInfo ] -> [ (String, ContractRef) ]
+contractsRefs = fmap mkContractRef . contractDefinitions
+  where
+    mkContractRef (SmallStatement _ (MkSmallStatement (Assign _ lhs rhs) _ _ _ _)) =
+      ( mkLhs lhs, mkRhs rhs)
+    mkContractRef _ = error "unexpected statement, should be something like 'xyz = Contract('xyz', '1.0.0')'"
+
+    mkLhs expr = expr ^. _Ident.identValue
+
+    mkRhs :: NonEmpty (a0, Expr '[] SrcInfo) -> ContractRef
+    mkRhs expr = case args of
+                   (c:v:_) -> ContractRef c v
+                   others  -> error $ "don't know how to extract contract ref from "  <> show others
+      where
+        args = fmap asString extArgs
+        rawArgs = fmap snd $ toList expr
+        extArgs = concatMap (toListOf (_Call.callArguments.folded.folded.argExpr)) $ rawArgs
+        asString e = showExpr e
+
+
+-- | Get contracts' imports
+schemaDefinitions :: [ Statement '[] SrcInfo ] -> [ Statement '[] SrcInfo ]
+schemaDefinitions = filter schemaImports
+  where
+    schemaImports (SmallStatement _ (MkSmallStatement asss _ _ _ _)) =
+      case asss ^? _Exprs._Call.callFunction._Ident.identValue of
+        Just "Schema" -> True
+        _             -> False
+    schemaImports _ = False
